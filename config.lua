c9d96a36cf58ca4afa664b8f108616f0b24c34b8
Jeremy <68263976+ansljeremy@users.noreply.github.com>
2021-09-15 05:48:44 -0400

    Fixed notification and grammar fixes

--- config.lua
+++ config.lua
@@ -11,11 +11,11 @@ Config.AllowedJobs = {
 }
 
 Config.messages = {
-  ['not_on_radio'] = 'Your not connected to a signal',
-  ['on_radio'] = 'Your already connected to this signal',
-  ['joined_to_radio'] = 'Your connected to: <b>',
+  ['not_on_radio'] = 'You\'re not connected to a signal',
+  ['on_radio'] = 'You\'re already connected to this signal',
+  ['joined_to_radio'] = 'You\'re connected to: ',
   ['restricted_channel_error'] = 'You can not connect to this signal!',
   ['invalid_radio'] = 'This frequency is not available.',
-  ['you_on_radio'] = 'Your already connected to this channel',
+  ['you_on_radio'] = 'You\'re already connected to this channel',
   ['you_leave'] = 'You left the channel.'
 }
