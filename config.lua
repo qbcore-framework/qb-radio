463698130320debe23b404a8241a7773a9b8f56a
ev.vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-09-28 23:41:24 +0300

    add Volume control with setRadioVolume

--- config.lua
+++ config.lua
@@ -1,26 +1,28 @@
 Config = {}
 
 Config.RestrictedChannels = {
-  [482] = {
-    police = true
-  },
-  [470] = {
-    ambulance = true
-  },
-  [858] = {
-    police = true,
-    ambulance = true
-  }
-} 
+    [482] = {
+        police = true
+    },
+    [470] = {
+        ambulance = true
+    },
+    [858] = {
+        police = true,
+        ambulance = true
+    }
+}
 
 Config.MaxFrequency = 500
 
 Config.messages = {
-  ['not_on_radio'] = 'You\'re not connected to a signal',
-  ['on_radio'] = 'You\'re already connected to this signal',
-  ['joined_to_radio'] = 'You\'re connected to: ',
-  ['restricted_channel_error'] = 'You can not connect to this signal!',
-  ['invalid_radio'] = 'This frequency is not available.',
-  ['you_on_radio'] = 'You\'re already connected to this channel',
-  ['you_leave'] = 'You left the channel.'
+    ["not_on_radio"] = "You're not connected to a signal",
+    ["on_radio"] = "You're already connected to this signal",
+    ["joined_to_radio"] = "You're connected to: ",
+    ["restricted_channel_error"] = "You can not connect to this signal!",
+    ["invalid_radio"] = "This frequency is not available.",
+    ["you_on_radio"] = "You're already connected to this channel",
+    ["you_leave"] = "You left the channel.",
+    ['decrease_radio_volume'] = 'The radio is already set to maximum volume',
+    ['increase_radio_volume'] = 'The radio is already set to the lowest volume'
 }
