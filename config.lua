ccc5af6bda58d82263794f7618fbff54963ae82f
Hyper <50504920+itsHyper@users.noreply.github.com>
2021-12-19 21:35:28 -0500

    PD / EMS More Realistic Radio Channels

--- config.lua
+++ config.lua
@@ -1,13 +1,43 @@
 Config = {}
 
 Config.RestrictedChannels = {
-    [482] = {
-        police = true
-    },
-    [470] = {
+    [1] = {
+        police = true,
         ambulance = true
     },
-    [858] = {
+    [2] = {
+        police = true,
+        ambulance = true
+    },
+    [3] = {
+        police = true,
+        ambulance = true
+    },
+    [4] = {
+        police = true,
+        ambulance = true
+    },
+    [5] = {
+        police = true,
+        ambulance = true
+    },
+    [6] = {
+        police = true,
+        ambulance = true
+    },
+    [7] = {
+        police = true,
+        ambulance = true
+    },
+    [8] = {
+        police = true,
+        ambulance = true
+    },
+    [9] = {
+        police = true,
+        ambulance = true
+    },
+    [10] = {
         police = true,
         ambulance = true
     }
