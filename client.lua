ee726914f7193432ce577062a25231708441b25c
Atikur Rahaman Santo <55808925+ARSSANTO@users.noreply.github.com>
2022-01-02 02:10:04 +0600

    localized and removed table insert

--- client.lua
+++ client.lua
@@ -5,6 +5,93 @@ local onRadio = false
 local RadioChannel = 0
 local RadioVolume = 50
 
+--Function
+local function LoadAnimDic(dict)
+    if not HasAnimDictLoaded(dict) then
+        RequestAnimDict(dict)
+        while not HasAnimDictLoaded(dict) do
+            Wait(0)
+        end
+    end
+end
+
+local function SplitStr(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        t[#t+1] = str
+    end
+    return t
+end
+
+local function connecttoradio(channel)
+    RadioChannel = channel
+    if onRadio then
+        exports["pma-voice"]:setRadioChannel(0)
+    else
+        onRadio = true
+        exports["pma-voice"]:setVoiceProperty("radioEnabled", true)
+    end
+    exports["pma-voice"]:setRadioChannel(channel)
+    if SplitStr(tostring(channel), ".")[2] ~= nil and SplitStr(tostring(channel), ".")[2] ~= "" then
+        QBCore.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. ' MHz', 'success')
+    else
+        QBCore.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. '.00 MHz', 'success')
+    end
+end
+
+local function closeEvent()
+	TriggerEvent("InteractSound_CL:PlayOnOne","click",0.6)
+end
+
+local function leaveradio()
+    closeEvent()
+    RadioChannel = 0
+    onRadio = false
+    exports["pma-voice"]:setRadioChannel(0)
+    exports["pma-voice"]:setVoiceProperty("radioEnabled", false)
+    QBCore.Functions.Notify(Config.messages['you_leave'] , 'error')
+end
+
+local function toggleRadioAnimation(pState)
+	LoadAnimDic("cellphone@")
+	if pState then
+		TriggerEvent("attachItemRadio","radio01")
+		TaskPlayAnim(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
+		radioProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, 1, 1, 0)
+		AttachEntityToEntity(radioProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, 1, 0, 0, 0, 2, 1)
+	else
+		StopAnimTask(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 1.0)
+		ClearPedTasks(PlayerPedId())
+		if radioProp ~= 0 then
+			DeleteObject(radioProp)
+			radioProp = 0
+		end
+	end
+end
+
+local function toggleRadio(toggle)
+    radioMenu = toggle
+    SetNuiFocus(radioMenu, radioMenu)
+    if radioMenu then
+        toggleRadioAnimation(true)
+        SendNUIMessage({type = "open"})
+    else
+        toggleRadioAnimation(false)
+        SendNUIMessage({type = "close"})
+    end
+end
+
+local function IsRadioOn()
+    return onRadio
+end
+
+--Exports
+exports("IsRadioOn", IsRadioOn)
+
+--Events
 AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
     PlayerData = QBCore.Functions.GetPlayerData()
 end)
@@ -28,69 +115,7 @@ RegisterNetEvent('qb-radio:onRadioDrop', function()
     end
 end)
 
--- Main Thread
-
-CreateThread(function()
-    while true do
-        Wait(1000)
-        if LocalPlayer.state.isLoggedIn and onRadio then
-            QBCore.Functions.TriggerCallback('qb-radio:server:GetItem', function(hasItem)
-                if not hasItem then
-                    if RadioChannel ~= 0 then
-                        leaveradio()
-                    end
-                end
-            end, "radio")
-        end
-    end
-end)
-
--- Functions
-
-function connecttoradio(channel)
-    RadioChannel = channel
-    if onRadio then
-        exports["pma-voice"]:setRadioChannel(0)
-    else
-        onRadio = true
-        exports["pma-voice"]:setVoiceProperty("radioEnabled", true)
-    end
-    exports["pma-voice"]:setRadioChannel(channel)
-    if SplitStr(tostring(channel), ".")[2] ~= nil and SplitStr(tostring(channel), ".")[2] ~= "" then
-        QBCore.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. ' MHz', 'success')
-    else
-        QBCore.Functions.Notify(Config.messages['joined_to_radio'] ..channel.. '.00 MHz', 'success')
-    end
-end
-
-function leaveradio()
-    closeEvent()
-    RadioChannel = 0
-    onRadio = false
-    exports["pma-voice"]:setRadioChannel(0)
-    exports["pma-voice"]:setVoiceProperty("radioEnabled", false)
-    QBCore.Functions.Notify(Config.messages['you_leave'] , 'error')
-end
-
-function SplitStr(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = {}
-    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
-        table.insert(t, str)
-    end
-    return t
-end
-
-function IsRadioOn()
-    return onRadio
-end
-
-exports("IsRadioOn", IsRadioOn)
-
 -- NUI
-
 RegisterNUICallback('joinRadio', function(data, cb)
     local rchannel = tonumber(data.channel)
     if rchannel ~= nil then
@@ -115,49 +140,6 @@ RegisterNUICallback('joinRadio', function(data, cb)
         QBCore.Functions.Notify(Config.messages['invalid_radio'] , 'error')
     end
 end)
-
-function LoadAnimDic(dict)
-    if not HasAnimDictLoaded(dict) then
-        RequestAnimDict(dict)
-        while not HasAnimDictLoaded(dict) do
-            Wait(0)
-        end
-    end
-end
-
-function toggleRadioAnimation(pState)
-	LoadAnimDic("cellphone@")
-	
-	if pState then
-		TriggerEvent("attachItemRadio","radio01")
-		TaskPlayAnim(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
-		radioProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, 1, 1, 0)
-		AttachEntityToEntity(radioProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, 1, 0, 0, 0, 2, 1)
-	else
-		StopAnimTask(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 1.0)
-		ClearPedTasks(PlayerPedId())
-		if radioProp ~= 0 then
-			DeleteObject(radioProp)
-			radioProp = 0
-		end
-	end
-end
-
-function closeEvent()
-	TriggerEvent("InteractSound_CL:PlayOnOne","click",0.6)
-end
-
-function toggleRadio(toggle)
-    radioMenu = toggle
-    SetNuiFocus(radioMenu, radioMenu)
-    if radioMenu then
-        toggleRadioAnimation(true)
-        SendNUIMessage({type = "open"})
-    else
-        toggleRadioAnimation(false)
-        SendNUIMessage({type = "close"})
-    end
-end
 
 RegisterNUICallback('leaveRadio', function(data, cb)
     if RadioChannel == 0 then
@@ -208,4 +190,20 @@ end)
 
 RegisterNUICallback('escape', function(data, cb)
     toggleRadio(false)
+end)
+
+--Main Thread
+CreateThread(function()
+    while true do
+        Wait(1000)
+        if LocalPlayer.state.isLoggedIn and onRadio then
+            QBCore.Functions.TriggerCallback('qb-radio:server:GetItem', function(hasItem)
+                if not hasItem then
+                    if RadioChannel ~= 0 then
+                        leaveradio()
+                    end
+                end
+            end, "radio")
+        end
+    end
 end)
