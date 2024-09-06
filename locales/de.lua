13f3e90d3a2c152965b39eda01a0fb0a1dcea913
Marcell <chillcode.codersparadise@gmail.com>
2024-09-06 14:00:23 +0200

    added locale de.lua

---
+++ locales/de.lua
@@ -0,0 +1,21 @@
+local Translations ={
+    ["not_on_radio"] = "Du bist nicht mit einem Signal verbunden",
+    ["on_radio"] = "Du bist bereits mit diesem Signal verbunden",
+    ["joined_to_radio"] = "Du bist verbunden mit: %{channel}",
+    ["restricted_channel_error"] = "Du kannst dich nicht mit diesem Signal verbinden!",
+    ["invalid_radio"] = "Diese Frequenz ist nicht verfügbar.",
+    ["you_on_radio"] = "Du bist bereits mit diesem Kanal verbunden",
+    ["you_leave"] = "Du hast den Kanal verlassen.",
+    ['volume_radio'] = 'Neue Lautstärke %{value}',
+    ['decrease_radio_volume'] = 'Das Radio ist bereits auf maximaler Lautstärke eingestellt',
+    ['increase_radio_volume'] = 'Das Radio ist bereits auf der niedrigsten Lautstärke eingestellt',
+    ['increase_decrease_radio_channel'] = 'Neuer Kanal %{value}',
+}
+
+if GetConvar('qb_locale', 'en') == 'de' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
