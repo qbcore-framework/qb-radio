fc12620b787b0c724b42f8a6d64a96fdffcfefe3
Cocodrulo <cocodrulojr@gmail.com>
2024-11-10 21:48:41 +0000

    Change number values to boolean values to match better the natives docs

--- client.lua
+++ client.lua
@@ -69,9 +69,9 @@ local function toggleRadioAnimation(pState)
 	LoadAnimDic("cellphone@")
 	if pState then
 		TriggerEvent("attachItemRadio","radio01")
-		TaskPlayAnim(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, 0, 0, 0)
-		radioProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, 1, 1, 0)
-		AttachEntityToEntity(radioProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, 1, 0, 0, 0, 2, 1)
+		TaskPlayAnim(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 2.0, 3.0, -1, 49, 0, false, false, false)
+		radioProp = CreateObject(`prop_cs_hand_radio`, 1.0, 1.0, 1.0, true, true, false)
+		AttachEntityToEntity(radioProp, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 57005), 0.14, 0.01, -0.02, 110.0, 120.0, -15.0, true, false, false, false, 2, true)
 	else
 		StopAnimTask(PlayerPedId(), "cellphone@", "cellphone_text_read_base", 1.0)
 		ClearPedTasks(PlayerPedId())
